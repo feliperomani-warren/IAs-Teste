0e5fce40e567fe81d957b477e295ade9a595660d
feliperomani-warren <felipe.romani@warren.com.br>
2025-06-13 17:11:47 -0300

    Update chat copy.py

--- pages /chat copy.py
+++ pages /chat copy.py
@@ -16,7 +16,7 @@ embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_a
 
 # Configuração da Página
 st.set_page_config(page_title="Chatbot Gemini com RAG", page_icon="🤖", layout="wide")
-st.logo("assets/LogoWarrenRena.png", icon_image="assets/LogoWRena.png")
+st.logo("LogoWarrenRena.png", icon_image="LogoWRena.png")
 st.title("🤖 Chatbot Gemini com RAG")
 
 # Inicialização do vetorstore no session_state
